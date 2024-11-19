d6b119d61470d102a4bf349b6192cabe58efc118
CeoMo <modiallo810@gmail.com>
2024-11-19 14:53:58 -0500

    Updated index.html

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -1,28 +1,60 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/js/utils.js":
-/*!*************************!*\
-  !*** ./src/js/utils.js ***!
-  \*************************/
-/***/ ((module) => {
+/***/ "./src/img/astronaut.gif":
+/*!*******************************!*\
+  !*** ./src/img/astronaut.gif ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-function distance(x1, y1, x2, y2) {
-  var xDist = x2 - x1;
-  var yDist = y2 - y1;
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-}
-module.exports = {
-  randomIntFromRange: randomIntFromRange,
-  randomColor: randomColor,
-  distance: distance
-};
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "b4dc19b81cf00e38f6fb487711b6ed20.gif");
+
+/***/ }),
+
+/***/ "./src/img/background.png":
+/*!********************************!*\
+  !*** ./src/img/background.png ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "9720169bb711f3c6b0ed0877c200bfa8.png");
+
+/***/ }),
+
+/***/ "./src/img/platform.png":
+/*!******************************!*\
+  !*** ./src/img/platform.png ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "7d125b1031f4a20ff86322b117ffa545.png");
+
+/***/ }),
+
+/***/ "./src/img/red_middle_animated.gif":
+/*!*****************************************!*\
+  !*** ./src/img/red_middle_animated.gif ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "485192536e64024c2afe154769d7e623.gif");
 
 /***/ })
 
@@ -53,18 +85,6 @@ module.exports = {
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
@@ -75,6 +95,18 @@ module.exports = {
 /******/ 				}
 /******/ 			}
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -93,89 +125,314 @@ module.exports = {
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl + "../";
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-(() => {
-"use strict";
 /*!**************************!*\
   !*** ./src/js/canvas.js ***!
   \**************************/
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _img_platform_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../img/platform.png */ "./src/img/platform.png");
+/* harmony import */ var _img_background_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../img/background.png */ "./src/img/background.png");
+/* harmony import */ var _img_astronaut_gif__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../img/astronaut.gif */ "./src/img/astronaut.gif");
+/* harmony import */ var _img_red_middle_animated_gif__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../img/red_middle_animated.gif */ "./src/img/red_middle_animated.gif");
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
+
+
+
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-var mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
-};
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
+canvas.width = 1024;
+canvas.height = 576;
+var gravity = 1.5;
+var levelCompleted = false;
+var levelAnnouncement = true;
+var animationId;
+var isPaused = false;
+var gameStarted = false;
 
-// Event Listeners
-addEventListener('mousemove', function (event) {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
-addEventListener('resize', function () {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  init();
-});
-
-// Objects
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+// Load the sunset GIF as a background
+var sunsetGif = new Image();
+sunsetGif.src = _img_red_middle_animated_gif__WEBPACK_IMPORTED_MODULE_3__["default"];
+var Player = /*#__PURE__*/function () {
+  function Player() {
+    _classCallCheck(this, Player);
+    this.position = {
+      x: 100,
+      y: 100
+    };
+    this.velocity = {
+      x: 0,
+      y: 1
+    };
+    this.width = 50;
+    this.height = 50;
+    this.image = new Image();
+    this.image.src = _img_astronaut_gif__WEBPACK_IMPORTED_MODULE_2__["default"];
   }
-  _createClass(Object, [{
+  _createClass(Player, [{
     key: "draw",
     value: function draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-      c.closePath();
+      c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
     }
   }, {
     key: "update",
     value: function update() {
       this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+      if (this.position.y + this.height + this.velocity.y <= canvas.height) this.velocity.y += gravity;else this.velocity.y = 0;
     }
   }]);
-  return Object;
-}(); // Implementation
-var objects;
-function init() {
-  objects = [];
-  for (var i = 0; i < 400; i++) {
-    // objects.push()
+  return Player;
+}();
+var Platform = /*#__PURE__*/function () {
+  function Platform(_ref) {
+    var x = _ref.x,
+      y = _ref.y,
+      image = _ref.image;
+    _classCallCheck(this, Platform);
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
+  }
+  _createClass(Platform, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    }
+  }]);
+  return Platform;
+}();
+var GenericObject = /*#__PURE__*/function () {
+  function GenericObject(_ref2) {
+    var x = _ref2.x,
+      y = _ref2.y,
+      image = _ref2.image;
+    _classCallCheck(this, GenericObject);
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
+  }
+  _createClass(GenericObject, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    }
+  }]);
+  return GenericObject;
+}();
+function createImage(imageSrc) {
+  var image = new Image();
+  image.src = imageSrc;
+  return image;
+}
+var platformImage = createImage(_img_platform_png__WEBPACK_IMPORTED_MODULE_0__["default"]);
+var backgroundImage = createImage(_img_background_png__WEBPACK_IMPORTED_MODULE_1__["default"]);
+var player, platforms, genericObjects, scrollOffset;
+function initializeGame() {
+  player = new Player();
+  scrollOffset = 0;
+  levelCompleted = false;
+  levelAnnouncement = true;
+
+  // Platforms aligned across the bottom of the canvas
+  platforms = [new Platform({
+    x: -1,
+    y: canvas.height - platformImage.height,
+    image: platformImage
+  }), new Platform({
+    x: platformImage.width - 3,
+    y: canvas.height - platformImage.height,
+    image: platformImage
+  }), new Platform({
+    x: platformImage.width * 2 - 6,
+    y: canvas.height - platformImage.height,
+    image: platformImage
+  }), new Platform({
+    x: platformImage.width * 3 - 9,
+    y: canvas.height - platformImage.height,
+    image: platformImage
+  }), new Platform({
+    x: platformImage.width * 4 - 12,
+    y: canvas.height - platformImage.height,
+    image: platformImage
+  }), new Platform({
+    x: platformImage.width * 5 - 15,
+    y: canvas.height - platformImage.height,
+    image: platformImage
+  })];
+  genericObjects = [new GenericObject({
+    x: 0,
+    y: 0,
+    image: backgroundImage
+  })];
+}
+var keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+};
+function animate() {
+  animationId = requestAnimationFrame(animate);
+
+  // Draw the sunset GIF as the background
+  c.drawImage(sunsetGif, 0, 0, canvas.width, canvas.height);
+
+  // Clear any previous drawings
+  c.fillStyle = 'white';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Draw other background objects and platforms
+  genericObjects.forEach(function (genericObject) {
+    return genericObject.draw();
+  });
+  platforms.forEach(function (platform) {
+    return platform.draw();
+  });
+  player.update();
+  if (levelAnnouncement) {
+    c.font = '30px Arial';
+    c.fillStyle = 'black';
+    c.fillText('Level 1', canvas.width / 2 - 50, 50);
+    setTimeout(function () {
+      return levelAnnouncement = false;
+    }, 2000);
+  }
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed && player.position.x > 100) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+    if (keys.right.pressed) {
+      scrollOffset += 5;
+      platforms.forEach(function (platform) {
+        return platform.position.x -= 5;
+      });
+      genericObjects.forEach(function (genericObject) {
+        return genericObject.position.x -= 2;
+      });
+    } else if (keys.left.pressed) {
+      scrollOffset -= 5;
+      platforms.forEach(function (platform) {
+        return platform.position.x += 5;
+      });
+      genericObjects.forEach(function (genericObject) {
+        return genericObject.position.x += 2;
+      });
+    }
+  }
+  platforms.forEach(function (platform) {
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+      player.velocity.y = 0;
+    }
+  });
+  if (scrollOffset > 2000 && !levelCompleted) {
+    levelCompleted = true;
+  }
+  if (levelCompleted) {
+    c.font = '30px Arial';
+    c.fillStyle = 'green';
+    c.fillText('Level 1 Completed!', canvas.width / 2 - 100, 100);
   }
 }
-
-// Animation Loop
-function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y);
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+function startGame() {
+  if (!gameStarted) {
+    initializeGame();
+    animate();
+    gameStarted = true;
+    isPaused = false;
+  } else if (isPaused) {
+    isPaused = false;
+    animate();
+  }
 }
-init();
-animate();
-})();
+function pauseGame() {
+  if (!isPaused) {
+    isPaused = true;
+    cancelAnimationFrame(animationId);
+  }
+}
+function resetGame() {
+  cancelAnimationFrame(animationId);
+  initializeGame();
+  gameStarted = false;
+  isPaused = false;
+  animate();
+}
 
+// Initialize game state on page load
+initializeGame();
+
+// Event listeners for controls
+document.getElementById('startButton').addEventListener('click', startGame);
+document.getElementById('pauseButton').addEventListener('click', pauseGame);
+document.getElementById('resetButton').addEventListener('click', resetGame);
+addEventListener('keydown', function (event) {
+  if (!isPaused) {
+    switch (event.key) {
+      case 'a':
+        keys.left.pressed = true;
+        break;
+      case 'd':
+        keys.right.pressed = true;
+        break;
+      case 'w':
+        if (player.velocity.y === 0) {
+          // Only jump if on a platform
+          player.velocity.y -= 20;
+        }
+        break;
+    }
+  }
+});
+addEventListener('keyup', function (event) {
+  switch (event.key) {
+    case 'a':
+      keys.left.pressed = false;
+      break;
+    case 'd':
+      keys.right.pressed = false;
+      break;
+  }
+});
 /******/ })()
 ;
 //# sourceMappingURL=canvas.bundle.js.map
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,237 @@
-import utils from './utils'
+import platformSrc from '../img/platform.png'
+import backgroundSrc from '../img/background.png'
+import astronautSrc from '../img/astronaut.gif'
+import sunsetGifSrc from '../img/red_middle_animated.gif' 
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = 1024
+canvas.height = 576
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+const gravity = 1.5
+let levelCompleted = false
+let levelAnnouncement = true
+let animationId
+let isPaused = false
+let gameStarted = false
+
+// Load the sunset GIF as a background
+const sunsetGif = new Image()
+sunsetGif.src = sunsetGifSrc
+
+class Player {
+    constructor() {
+        this.position = { x: 100, y: 100 }
+        this.velocity = { x: 0, y: 1 }
+        this.width = 50
+        this.height = 50
+        this.image = new Image()
+        this.image.src = astronautSrc
+    }
+    
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
+    
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y 
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) 
+            this.velocity.y += gravity
+        else 
+            this.velocity.y = 0
+    }
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+class Platform {
+    constructor({ x, y, image }) {
+        this.position = { x, y }
+        this.image = image
+        this.width = image.width
+        this.height = image.height
+    }
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
-
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
-
-  init()
-})
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
-  }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
+class GenericObject {
+    constructor({ x, y, image }) {
+        this.position = { x, y }
+        this.image = image
+        this.width = image.width
+        this.height = image.height
+    }
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
 }
 
-// Animation Loop
+function createImage(imageSrc) {
+    const image = new Image()
+    image.src = imageSrc
+    return image
+}
+
+const platformImage = createImage(platformSrc)
+const backgroundImage = createImage(backgroundSrc)
+
+let player, platforms, genericObjects, scrollOffset
+
+function initializeGame() {
+    player = new Player()
+    scrollOffset = 0
+    levelCompleted = false
+    levelAnnouncement = true
+
+    // Platforms aligned across the bottom of the canvas
+    platforms = [
+        new Platform({ x: -1, y: canvas.height - platformImage.height, image: platformImage }),
+        new Platform({ x: platformImage.width - 3, y: canvas.height - platformImage.height, image: platformImage }),
+        new Platform({ x: platformImage.width * 2 - 6, y: canvas.height - platformImage.height, image: platformImage }),
+        new Platform({ x: platformImage.width * 3 - 9, y: canvas.height - platformImage.height, image: platformImage }),
+        new Platform({ x: platformImage.width * 4 - 12, y: canvas.height - platformImage.height, image: platformImage }),
+        new Platform({ x: platformImage.width * 5 - 15, y: canvas.height - platformImage.height, image: platformImage })
+    ]
+
+    genericObjects = [ 
+        new GenericObject({ x: 0, y: 0, image: backgroundImage })
+    ]
+}
+
+const keys = {
+    right: { pressed: false },
+    left: { pressed: false }
+}
+
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+    animationId = requestAnimationFrame(animate)
+    
+    // Draw the sunset GIF as the background
+    c.drawImage(sunsetGif, 0, 0, canvas.width, canvas.height)
+    
+    // Clear any previous drawings
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+    // Draw other background objects and platforms
+    genericObjects.forEach(genericObject => genericObject.draw())
+    platforms.forEach(platform => platform.draw())
+    
+    player.update()
+    
+    if (levelAnnouncement) {
+        c.font = '30px Arial'
+        c.fillStyle = 'black'
+        c.fillText('Level 1', canvas.width / 2 - 50, 50)
+        setTimeout(() => levelAnnouncement = false, 2000)
+    }
+
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5
+    } else {
+        player.velocity.x = 0
+
+        if (keys.right.pressed) {
+            scrollOffset += 5
+            platforms.forEach(platform => platform.position.x -= 5)
+            genericObjects.forEach(genericObject => genericObject.position.x -= 2)
+        } else if (keys.left.pressed) {
+            scrollOffset -= 5
+            platforms.forEach(platform => platform.position.x += 5)
+            genericObjects.forEach(genericObject => genericObject.position.x += 2)
+        }
+    }
+
+    platforms.forEach(platform => {
+        if (
+            player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width
+        ) {
+            player.velocity.y = 0
+        }
+    })
+
+    if (scrollOffset > 2000 && !levelCompleted) {
+        levelCompleted = true
+    }
+
+    if (levelCompleted) {
+        c.font = '30px Arial'
+        c.fillStyle = 'green'
+        c.fillText('Level 1 Completed!', canvas.width / 2 - 100, 100)
+    }
 }
 
-init()
-animate()
+function startGame() {
+    if (!gameStarted) {
+        initializeGame()
+        animate()
+        gameStarted = true
+        isPaused = false
+    } else if (isPaused) {
+        isPaused = false
+        animate()
+    }
+}
+
+function pauseGame() {
+    if (!isPaused) {
+        isPaused = true
+        cancelAnimationFrame(animationId)
+    }
+}
+
+function resetGame() {
+    cancelAnimationFrame(animationId)
+    initializeGame()
+    gameStarted = false
+    isPaused = false
+    animate()
+}
+
+// Initialize game state on page load
+initializeGame()
+
+// Event listeners for controls
+document.getElementById('startButton').addEventListener('click', startGame)
+document.getElementById('pauseButton').addEventListener('click', pauseGame)
+document.getElementById('resetButton').addEventListener('click', resetGame)
+
+addEventListener('keydown', (event) => {
+    if (!isPaused) {
+        switch (event.key) {
+            case 'a':
+                keys.left.pressed = true
+                break
+            case 'd':
+                keys.right.pressed = true
+                break
+            case 'w':
+                if (player.velocity.y === 0) { // Only jump if on a platform
+                    player.velocity.y -= 20
+                }
+                break
+        }
+    }
+})
+
+addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'a':
+            keys.left.pressed = false
+            break
+        case 'd':
+            keys.right.pressed = false
+            break
+    }
+})
